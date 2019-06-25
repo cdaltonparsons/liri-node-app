a07e97ee9ea4d5e8e5b887da2d39db66f55d8d8c
Charlie Parsons <cdaltonparsons@gmail.com>
2019-06-25 17:57:39 -0600

    fixed the problem with multiple responses on the spotify function call, and also applied a when for the second inquirer prompt

--- liri.js
+++ liri.js
@@ -23,7 +23,11 @@ inquirer
     },
     {
       type: "input",
-      message: "And what song, band, or movie would you like me to look up? (Please no special characters, i.e. periods (.) or apostrophes ('))",
+      message:
+        "And what song, band, or movie would you like me to look up? (Please no special characters, i.e. periods (.) or apostrophes ('))",
+      when: function(userInput) {
+        return userInput.action !== "do-what-it-says";
+      },
       name: "thing"
     }
   ])
@@ -93,75 +97,86 @@ function spotifyThis(answer) {
             response.tracks.items[0].album.name +
             "\n"
         );
+        return;
       }
     });
-  } spotify.search({ type: "track", query: "the sign" }, function(err, response) {
-    if (err) {
-      return console.log("Error occurred: " + err);
-    } else {
-    console.log(
-      "\nYou forgot to input a song, so here is information on my favorite song, 'The Sign is by: " + response.tracks.items[5].artists[0].name
-    );
-    console.log("\n");
-    console.log("The track name is: " + response.tracks.items[5].name);
-    console.log("\n");
-    console.log(
-      "Here's a link to the song on spotify: " +
-        response.tracks.items[5].external_urls.spotify
-    );
-    console.log("\n");
-    console.log(
-      "It appears on the album: " + response.tracks.items[5].album.name + "\n"
-    );
+  } else {
+    spotify.search({ type: "track", query: "the sign" }, function(
+      err,
+      response
+    ) {
+      if (err) {
+        return console.log("Error occurred: " + err);
+      } else {
+        console.log(
+          "\nYou forgot to input a song, so here is information on my favorite song, 'The Sign is by: " +
+            response.tracks.items[5].artists[0].name
+        );
+        console.log("\n");
+        console.log("The track name is: " + response.tracks.items[5].name);
+        console.log("\n");
+        console.log(
+          "Here's a link to the song on spotify: " +
+            response.tracks.items[5].external_urls.spotify
+        );
+        console.log("\n");
+        console.log(
+          "It appears on the album: " +
+            response.tracks.items[5].album.name +
+            "\n"
+        );
+      }
+    });
   }
-})
 }
 
 function movieThis(answer) {
   if (answer) {
-  var queryUrlMovies =
-    "http://www.omdbapi.com/?t=" + answer + "&y=&plot=short&apikey=trilogy";
-  axios.get(queryUrlMovies).then(function(response) {
-    console.log("\n");
-    console.log(
-      `The movie name is ${response.data.Title}, it was released in ${
-        response.data.Year
-      }.  It has an IMDB rating of ${
-        response.data.imdbRating
-      }, and a Rotten Tomato rating of ${
-        response.data.Ratings[1].Value
-      }.  It was produced in ${
-        response.data.Country
-      }, and the spoken language in the movie is ${response.data.Language}.`
-    );
-    console.log("\n");
-    console.log(`Here's a brief plot summary: ${response.data.Plot}`);
-    console.log("\n");
-    console.log(`Headlining actors: ${response.data.Actors}`);
-  });
-} else {
-  var queryUrlMovies =
-    "http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy";
-  axios.get(queryUrlMovies).then(function(response) {
-    console.log("\nYou forgot to enter a movie title, so here's some information on a movie I've never heard of");
-    console.log(
-      `The movie name is ${response.data.Title}, it was released in ${
-        response.data.Year
-      }.  It has an IMDB rating of ${
-        response.data.imdbRating
-      }, and a Rotten Tomato rating of ${
-        response.data.Ratings[1].Value
-      }.  It was produced in ${
-        response.data.Country
-      }, and the spoken language in the movie is ${response.data.Language}.`
-    );
-    console.log("\n");
-    console.log(`Here's a brief plot summary: ${response.data.Plot}`);
-    console.log("\n");
-    console.log(`Headlining actors: ${response.data.Actors}`);
-})
+    var queryUrlMovies =
+      "http://www.omdbapi.com/?t=" + answer + "&y=&plot=short&apikey=trilogy";
+    axios.get(queryUrlMovies).then(function(response) {
+      console.log("\n");
+      console.log(
+        `The movie name is ${response.data.Title}, it was released in ${
+          response.data.Year
+        }.  It has an IMDB rating of ${
+          response.data.imdbRating
+        }, and a Rotten Tomato rating of ${
+          response.data.Ratings[1].Value
+        }.  It was produced in ${
+          response.data.Country
+        }, and the spoken language in the movie is ${response.data.Language}.`
+      );
+      console.log("\n");
+      console.log(`Here's a brief plot summary: ${response.data.Plot}`);
+      console.log("\n");
+      console.log(`Headlining actors: ${response.data.Actors}`);
+    });
+  } else {
+    var queryUrlMovies =
+      "http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy";
+    axios.get(queryUrlMovies).then(function(response) {
+      console.log(
+        "\nYou forgot to enter a movie title, so here's some information on a movie I've never heard of"
+      );
+      console.log(
+        `The movie name is ${response.data.Title}, it was released in ${
+          response.data.Year
+        }.  It has an IMDB rating of ${
+          response.data.imdbRating
+        }, and a Rotten Tomato rating of ${
+          response.data.Ratings[1].Value
+        }.  It was produced in ${
+          response.data.Country
+        }, and the spoken language in the movie is ${response.data.Language}.`
+      );
+      console.log("\n");
+      console.log(`Here's a brief plot summary: ${response.data.Plot}`);
+      console.log("\n");
+      console.log(`Headlining actors: ${response.data.Actors}`);
+    });
+  }
 }
-};
 
 function doWhat() {
   fs.readFile("random.txt", "utf8", function(err, response) {
